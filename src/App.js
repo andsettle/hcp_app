91d8a03518690e4e3c87c1091f16ba1b3c95b12e
Andrew Settle <andsettle1@gmail.com>
2022-05-25 12:27:53 -0700

    initial push of test code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const getURL = "https://jsonplaceholder.typicode.com/users";
+  const postURL =
+    "https://dev.app.homecarepulse.com/Primary/?FlowId=7423bd80-cddb-11ea-9160-326dddd3e106&Action=api";
+
+  const onSuccess = (response) => {
+    return response.json().then((data) => {
+      const users = data.map(mapUsers);
+      console.log(users);
+
+      let fetchData = {
+        method: "POST",
+        body: JSON.stringify(
+          {
+            userid: "andsettle1@gmail.com",
+            password: "3d0612aa153448d3b8231d766e6ea910",
+            outputtype: "Json",
+          } + users
+        ),
+        headers: new Headers({
+          "Content-Type": "application/json; charset=UTF-8",
+        }),
+      };
+
+      fetch(postURL, fetchData).then(onError);
+    });
+  };
+
+  const mapUsers = (user) => {
+    let newUsers = {
+      first_name: user.name.split(" ")[0],
+      last_name: user.name.split(" ")[1],
+      company_name: user.company.name,
+      company_full_address:
+        user.address.street +
+        ", " +
+        user.address.suite +
+        ", " +
+        user.address.city +
+        ", " +
+        user.address.zipcode,
+      website: user.website,
+      phone: user.phone,
+    };
+    return newUsers;
+  };
+
+  const onError = (err) => {
+    console.log(err);
+  };
+
+  fetch(getURL).then(onSuccess).catch(onError);
+
   return (
     <div className="App">
       <header className="App-header">
